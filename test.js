bfba99e211852477b28b97942fb968e099549eca
John Titus <john.titus@trustedconcepts.com>
2014-10-15 13:04:21 -0400

    added visible check adapted from jquery https://github.com/jquery/jquery/blob/10399ddcf8a239acc27bdec9231b996b178224d3/src/css/hiddenVisibleSelectors.js
    removed cut&pasted code
    
    removed test file
    
    Updated readme.
    
    removed spaces in test

--- test.js
+++
@@ -1,9 +0,0 @@
-var Nightmare = require("./lib/index");
-
-new Nightmare()
-	.agent("firefox")
-	.goto("https://www.google.com")
-	.visible("#gs_sc0", function( exists){
-		console.log( "vis? " + exists);
-	})
-	.run();
