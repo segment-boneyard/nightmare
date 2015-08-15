df983ddad5795d0a8feea2f094660dcc0c1cf1e8
Matthew Mueller <mattmuelle@gmail.com>
2015-08-15 00:30:03 -0500

    rename to nightmare

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
-var Nightmare = require('nightmare')
-var Stellar = require('./');
+var Nightmare = require('./');
 var vo = require('vo')
 
 vo(run)(function(err, result) {
@@ -8,45 +7,22 @@ vo(run)(function(err, result) {
 })
 
 function *run() {
-  var stellar = Stellar();
+  var nightmare = Nightmare();
 
-  var first = yield stellar
+  var first = yield nightmare
     .goto('http://cnn.com')
     .evaluate(function() {
       return document.title;
     })
 
-  var second = yield stellar
+  var second = yield nightmare
     .goto('http://facebook.com')
     .evaluate(function() {
       return document.title;
     })
 
   // disconnect
-  yield stellar.end()
+  yield nightmare.end()
 
   return first + ' | ' + second;
 }
-
-// var nightmare = new Nightmare();
-
-// console.time('nightmare');
-// nightmare
-//   .goto('http://cnn.com')
-//   .evaluate(function() {
-//     return document.title;
-//   })
-//   .run(function(err, value) {
-//     console.timeEnd('nightmare');
-//   })
-
-// function *run() {
-//   var title = yield Stellar()
-//     .goto('http://yahoo.com')
-//     .type('input[title="Search"]', 'github nightmare')
-//     .click('.searchsubmit')
-
-//   yield stellar.end();
-
-//   return title;
-// }
