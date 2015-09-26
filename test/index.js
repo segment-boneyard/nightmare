d3d8e765f93c8eedc78a8d4f01b04e82e1be0aa4
Fritz Lin <uxfritz@163.com>
2015-09-26 13:13:47 +0800

    changing the tests to point to github to avoid the great firewall fix #249

--- test/index.js
+++ test/index.js
@@ -325,7 +325,7 @@ describe('Nightmare', function () {
     it('should take a screenshot', function*() {
       yield mkdirp('/tmp/nightmare');
       yield nightmare
-        .goto('http://google.com/')
+        .goto('https://github.com/')
         .screenshot('/tmp/nightmare/test.png');
       var stats = fs.statSync('/tmp/nightmare/test.png');
       stats.size.should.be.at.least(1000);
@@ -334,7 +334,7 @@ describe('Nightmare', function () {
     it('should take a clipped screenshot', function*() {
       yield mkdirp('/tmp/nightmare');
       yield nightmare
-        .goto('http://google.com/')
+        .goto('https://github.com/')
         .screenshot('/tmp/nightmare/test-clipped.png', {
           x: 200,
           y: 100,
