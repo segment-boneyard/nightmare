9db8b5c993b2f795af16af8f7d9c9757c326f8f2
Sean McLellan <sean.mclellan@baristalabs.com>
2016-04-05 09:50:45 -0400

    forward port the synchronized screenshot merge

--- test/index.js
+++ test/index.js
@@ -12,6 +12,7 @@ var server = require('./server');
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 var path = require('path');
+var PNG = require('pngjs').PNG;
 var rimraf = require('rimraf');
 var child_process = require('child_process');
 var should = chai.should();
@@ -848,6 +849,41 @@ describe('Nightmare', function () {
             Buffer.isBuffer(image).should.be.true;
             image.length.should.be.at.least(300);
         });
+
+        // repeat this test 3 times, since the concern here is non-determinism in
+        // the timing accuracy of screenshots -- it might pass once, but likely not
+        // several times in a row.
+        for (var i = 0; i < 3; i++) {
+            it('should screenshot an up-to-date image of the page (' + i + ')', function* () {
+                var image = yield nightmare.chain()
+                    .goto('about:blank')
+                    .viewport(100, 100)
+                    .evaluate(function () { document.body.style.background = '#900'; })
+                    .evaluate(function () { document.body.style.background = '#090'; })
+                    .screenshot();
+
+                var png = new PNG();
+                var imageData = yield png.parse.bind(png, image);
+                var firstPixel = Array.from(imageData.data.slice(0, 3));
+                firstPixel.should.deep.equal([0, 153, 0]);
+            });
+        }
+
+        it('should screenshot an idle page', function* () {
+            var image = yield nightmare.chain()
+                .goto('about:blank')
+                .viewport(100, 100)
+                .evaluate(function () { document.body.style.background = '#900'; })
+                .evaluate(function () { document.body.style.background = '#090'; })
+                .wait(1000)
+                .screenshot();
+
+            var png = new PNG();
+            var imageData = yield png.parse.bind(png, image);
+            var firstPixel = Array.from(imageData.data.slice(0, 3));
+            firstPixel.should.deep.equal([0, 153, 0]);
+        });
+
 
         it('should load jquery correctly', function* () {
             var loaded = yield nightmare.chain()
