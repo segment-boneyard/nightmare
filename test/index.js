7bc25cb31670cec473256946835b46017d649bea
Philip Ingrey <phil.ingrey@digitalanimal.com>
2016-04-06 09:14:03 +0100

    add test

--- test/index.js
+++ test/index.js
@@ -65,6 +65,16 @@ describe('Nightmare', function () {
     });
   });
 
+  it('should exit with a non-zero code on uncaughtExecption', function(done) {
+    var child = child_process.fork(
+      path.join(__dirname, 'files', 'nightmare-error.js'), [], {silent: true});
+
+      child.once('exit', function(code) {
+        code.should.not.equal(0);
+        done();
+      });
+  });
+
   describe('navigation', function () {
     var nightmare;
 
