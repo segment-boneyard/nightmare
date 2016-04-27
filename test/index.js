e31f94f1235aec7e7d3ee57fc3227b9e6ad54da4
ross <rosshinkley@gmail.com>
2016-04-27 00:09:28 -0400

    adding synchronous version retrieval

--- test/index.js
+++ test/index.js
@@ -54,6 +54,15 @@ describe('Nightmare', function () {
     yield nightmare.end();
   });
 
+  it('should have version information', function*() {
+    var nightmare = Nightmare();
+    nightmare.should.be.ok;
+    nightmare.engineVersions.electron.should.be.ok;
+    nightmare.engineVersions.chrome.should.be.ok;
+    nightmare.version.should.be.ok;
+    yield nightmare.end();
+  });
+
   it('should kill its electron process when it is killed', function(done) {
     var child = child_process.fork(
       path.join(__dirname, 'files', 'nightmare-unended.js'));
